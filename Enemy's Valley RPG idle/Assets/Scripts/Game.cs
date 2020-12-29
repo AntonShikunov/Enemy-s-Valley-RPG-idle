1c1867805c0a3b8b299c8e9cb4316add96621d72
AntonShikunov <lio_polo@bk.ru>
2020-12-29 21:44:31 +0300

    something new

--- Enemy's Valley RPG idle/Assets/Scripts/Game.cs
+++ Enemy's Valley RPG idle/Assets/Scripts/Game.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+//using System.Xml;
+//using GooglePlayGames;
+//using UnityEngine.SocialPlatforms;
+//using GooglePlayGames.BasicApi;
+using UnityEngine.Advertisements;
 public class Game : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject prefab_enemy; // префаб противника. Создаются его копии
+
+    public GameObject BGFight; // родной фон для префаба противника
+
+    public Sprite[] EnemiesSprite = new Sprite[4]; // массив спрайтов противников
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
+    }
+
+    public void GeneratorEnemy()
+    {
+        GameObject enemyObj = Instantiate(prefab_enemy, prefab_enemy.transform.position, Quaternion.identity) as GameObject;
+        int rand = Random.Range(0, 4);
+        enemyObj.GetComponent<Image>().sprite = EnemiesSprite[rand];
+        enemyObj.transform.SetParent(BGFight.transform);
+        enemyObj.transform.localPosition = new Vector3(Random.Range(-290, 210), Random.Range(370, -210), 0);
+        enemyObj.transform.localScale = new Vector3(1f, 1f, 1f);
     }
 }
