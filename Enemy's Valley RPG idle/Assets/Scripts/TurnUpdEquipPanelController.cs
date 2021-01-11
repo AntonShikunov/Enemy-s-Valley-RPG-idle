693159436021f2d8e4dd9e044059ac2dbbf3db1a
Evgeniy <tuq_solo@mail.ru>
2021-01-11 20:56:06 +0300

    UpdEquipPanel

---
+++ Enemy's Valley RPG idle/Assets/Scripts/TurnUpdEquipPanelController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class TurnUpdEquipPanelController : MonoBehaviour
+{
+    public GameObject gameObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void TurnOnUpdEquipPanelBttn()
+    {
+        gameObject.GetComponent<Animation>().Play("TurnOnUpdEquipPanel");
+    }
+    public void TurnOffUpdEquipPanelBttn()
+    {
+        gameObject.GetComponent<Animation>().Play("TurnOffUpdEquipPanel");
+    }
+}
